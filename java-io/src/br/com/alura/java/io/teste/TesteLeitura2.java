df70134924cca01c04314c3de211e8269d352e4c
Nicollas Martins <nicollasafmartins@gmail.com>
2022-08-03 20:09:32 -0300

    updating

--- java-io/src/br/com/alura/java/io/teste/TesteLeitura2.java
+++ java-io/src/br/com/alura/java/io/teste/TesteLeitura2.java
@@ -6,36 +6,31 @@ import java.util.Scanner;
 
 public class TesteLeitura2 {
 
-	public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
 
-		Scanner scanner = new Scanner(new File("contas.csv"));
+        Scanner scanner = new Scanner(new File("contas.csv"));
+        while(scanner.hasNextLine()) {
+                String linha = scanner.nextLine();
+                System.out.println(linha);
 
-//		String linha = scanner.nextLine();
-//		System.out.println(linha);
+                Scanner linhaScanner = new Scanner(linha);
+                linhaScanner.useLocale(Locale.US);
+                linhaScanner.useDelimiter(",");
 
-		while (scanner.hasNextLine()) {
-			String linha = scanner.nextLine();
-			System.out.println(linha);
+                String valor1 = linhaScanner.next();
+                int valor2 = linhaScanner.nextInt();
+                int valor3 = linhaScanner.nextInt();
+                String valor4 = linhaScanner.next();
+                double valor5 = linhaScanner.nextDouble();
 
-			Scanner linhaScanner = new Scanner(linha);
-			linhaScanner.useLocale(Locale.US);
-			linhaScanner.useDelimiter(",");
+                System.out.println(valor1 + valor2 + valor3 + valor4 + valor5);
 
-			String valor1 = linhaScanner.next();
-			int valor2 = linhaScanner.nextInt();
-			int valor3 = linhaScanner.nextInt();
-			String valor4 = linhaScanner.next();
-			double valor5 = linhaScanner.nextDouble();
+                linhaScanner.close();
 
-			System.out.println(valor1 + valor2 + valor3 + valor4 + valor5);
-
-			linhaScanner.close();
-
-//			String[] valores = linha.split(",");
-// 			System.out.println(Arrays.toString(valores));
-//			System.out.println(valores[3]);
-		}
-		scanner.close();
-	}
+//                String[] valores = linha.split(",");
+//                System.out.println(valores[1]);
+        }
+        scanner.close();
+}
 
 }
