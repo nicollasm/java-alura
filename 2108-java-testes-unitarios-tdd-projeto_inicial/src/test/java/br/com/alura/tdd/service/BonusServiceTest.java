3bdd89d4429c1cda7e31392a1f98eaec907a6015
Nicollas Martins <nicollasafmartins@gmail.com>
2022-08-26 14:53:38 -0300

    junit study

---
+++ 2108-java-testes-unitarios-tdd-projeto_inicial/src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
@@ -0,0 +1,22 @@
+package br.com.alura.tdd.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.Test;
+
+import br.com.alura.tdd.modelo.Funcionario;
+
+class BonusServiceTest {
+
+    @Test
+    void bonusDeveriaSerZeroParaFuncionarioComSalarioMuitoAlto() {
+        BonusService service = new BonusService();
+            service.calcularBonus (new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("25000")));
+
+        assertEquals(BigDecimal.ZERO, bonus);
+    }
+
+}
